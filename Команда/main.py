3e8c20a86a3af29f6bafde174870848548299ebe
dis_verb. <mail_rop@mail.ru>
2024-03-31 14:06:08 +0300

    modified:   "\320\232\320\276\320\274\320\260\320\275\320\264\320\260/main.py"

--- Команда/main.py
+++ Команда/main.py
@@ -5,3 +5,13 @@ def add(a, b):
 # Функция для вычисления разности двух чисел
 def subtract(a, b):
     return a - b
+
+# Функция для определения четности числа
+def is_even(num):
+    return num % 2 == 0
+
+# Функция для вычисления факториала числа
+def factorial(n):
+    if n == 0:
+        return 1
+        return n * factorial(n-1)
